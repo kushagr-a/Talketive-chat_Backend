a82fe26fadb551b0c861dc1f2d1fe0d472c28287
Kushagra bharti <kushagrabharti16@gmail.com>
2025-06-30 21:54:13 +0530

    update cors

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://talketive-chat-frontend.vercel.app",
+      "https://talketive-chat-frontend-eijy.vercel.app/",
     ], // Allow local and deployed frontend origins
     credentials: true,
   })
